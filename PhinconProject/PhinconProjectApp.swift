025ad0f866f82000deaac385c0b4aaeb68beb1c6
ekapradityagk <ekapradityagk@gmail.com>
2024-08-08 11:23:01 +0700

    Initial Commit

---
+++ PhinconProject/PhinconProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhinconProjectApp.swift
+//  PhinconProject
+//
+//  Created by Eka Praditya on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PhinconProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
